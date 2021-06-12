a57834e91a1b073e8497118ef818a11e893c1c54
RickyAnchores <ricky23anchores@gmail.com>
2021-06-12 15:46:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+
+      <h1>React-Nova</h1>
+      <p>Personal React Boiler Plate</p>
+      <h3>This contains:</h3>
+      <ul>
+        <li>Basic Structure</li>
+        <li>Basic Components</li>
+        <li>SASS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar.scss";
+
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
