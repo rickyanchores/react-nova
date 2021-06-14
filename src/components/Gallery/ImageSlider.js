309bda46d9b9f6fc888e3a7ccff4c66f3951ae12
RickyAnchores <ricky23anchores@gmail.com>
2021-06-14 19:28:46 +0100

    added gallery

--- src/components/Gallery/ImageSlider.js
+++ src/components/Gallery/ImageSlider.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import "./Gallery.scss";
 import { SliderData } from './SliderData';
 import {FaArrowAltCircleRight,FaArrowAltCircleLeft} from 'react-icons/fa';
 
@@ -6,15 +7,34 @@ const ImageSlider = ({ slides }) => {
     const [current,setCurrent] = useState(0)
     const length = slides.length;
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0){
+        return null;
+    }
+
+
+
+
     return (
         <section className="slider"> 
-            <FaArrowAltCircleLeft className="left-arrow" />
-            <FaArrowAltCircleRight className="right-arrow" />
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
 
 
             {SliderData.map((slide, index) => {
                 return(
-                    <img src={slide.image} alt="" className="image"/>
+                   <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                        {index === current && (
+                            <img src={slide.image} alt="" className="image"/>
+                        )}
+                   </div>
                 )
             })}
         </section>
