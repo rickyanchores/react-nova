0463377f5bfaee133a92ca8083e35fb9b299c820
RickyAnchores <ricky23anchores@gmail.com>
2021-06-18 17:38:11 +0100

    added img in Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Banner = () => {
+
+    function changeBg(){
+
+    let BannerBg = document.querySelector('.Banner');
+
+    BannerBg.addEventListener('click',() => {
+        BannerBg.style.background = "teal";
+    })
+
+}
+
     return (
         <div className="Banner">
             <h1>Banner</h1>
-            <img src="" alt="" />
+            <img src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmVvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="" />
         </div>
     )
 }
